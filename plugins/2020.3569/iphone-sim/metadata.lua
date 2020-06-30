6ff4cb524fe6e3292214a84b9750f559f89bd716
Vlad Shcherban <vlad@coronalabs.com>
2020-06-30 10:58:09 +0300

    Updating binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDTwitterMoPubAdapter', },
+		frameworks = { 'MoPub', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata

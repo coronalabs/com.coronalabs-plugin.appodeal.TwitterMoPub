29bb9e514546e7d0d9d03fae633cf0b398c93598
Vlad Shcherban <vlad@coronalabs.com>
2021-08-16 20:12:28 +0300

    Updating to 2.10.1

--- plugins/2018.3326/iphone-sim/metadata.lua
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDTwitterMoPubAdapter', 'Avid-mopub-3.6.1', 'mopub-ios-sdk' },
-		frameworks = { 'MPUBMoatMobileAppKit', },
+		staticLibs = { 'APDTwitterMoPubAdapter', },
+		frameworks = { 'MoPub', },
 		frameworksOptional = {},
 	},
 }
